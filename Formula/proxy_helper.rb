6bbf1e04e2271119a2ef1911d2c51d099598f136
Takuma Watanabe <takumaw@sfo.kuramae.ne.jp>
2018-12-29 15:44:08 +0900

    Updated to 0.0.4. Changed method to build the binary.

--- Formula/proxy_helper.rb
+++ Formula/proxy_helper.rb
@@ -1,9 +1,9 @@
 class ProxyHelper < Formula
   desc "Helper for constructing proxy environment variables"
   homepage "https://github.com/takumaw/proxy_helper"
-  url "https://github.com/takumaw/proxy_helper/archive/0.0.3.tar.gz"
-  version "0.0.3"
-  sha256 "61fbc804155a1743961184a65425c2d45e01d4dc2600d7b8b02db7c2ff1e651a"
+  url "https://github.com/takumaw/proxy_helper/archive/0.0.4.tar.gz"
+  version "0.0.4"
+  sha256 "ec26ca9680996450ee828a3160411a73cdf710e9615027d7858a48c80275f2db"
   head "https://github.com/takumaw/proxy_helper.git"
 
   depends_on "automake" => :build
@@ -14,19 +14,19 @@ class ProxyHelper < Formula
     system "automake", "--add-missing"
     system "autoconf"
     system "./configure", "--prefix=#{prefix}"
-    system "make", "install"
+    system "env", "SWIFTBBUILDFLAGS=--disable-sandbox", "make", "install"
   end
 
   def caveats; <<~EOS
-    proxy_helper has been installed as
+    proxy_helper has been installed at
 
       #{HOMEBREW_PREFIX}/opt/proxy_helper/libexec/proxy_helper
 
-    You can enable proxy_helper by adding the following lines to
-    your `/etc/profile` and/or `/etc/zprofile`:
+    You can enable proxy_helper by adding the following code snippet to
+    your `/etc/profile` for BASH, or `/etc/zprofile` for ZSH:
 
       if [ -x #{HOMEBREW_PREFIX}/opt/proxy_helper/libexec/proxy_helper ]; then
-        eval `#{HOMEBREW_PREFIX}/opt/proxy_helper/libexec/proxy_helper -s`
+          eval `#{HOMEBREW_PREFIX}/opt/proxy_helper/libexec/proxy_helper -s`
       fi
   EOS
   end
